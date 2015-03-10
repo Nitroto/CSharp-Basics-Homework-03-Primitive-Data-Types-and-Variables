c66d45fcb679134347e42341c9485d1dac7f3a19
Nitroto <astodorov@outlook.com>
2015-03-10 15:39:52 +0200

    Create 10.EmployeeData.cs

---
+++ 10.EmployeeData.cs
@@ -0,0 +1,20 @@
+using System;
+
+class EmployeeData
+{
+    static void Main()
+    {
+        string firstName = Console.ReadLine();
+        string lastName = Console.ReadLine();
+        ushort age = Convert.ToUInt16(Console.ReadLine());
+        string gender = Console.ReadLine();
+        ulong idNum = Convert.ToUInt64(Console.ReadLine());
+        ulong empNum = Convert.ToUInt64(Console.ReadLine());
+        Console.WriteLine("Name: " + firstName);
+        Console.WriteLine("Last Name: " + lastName);
+        Console.WriteLine("Age: " + age);
+        Console.WriteLine("Gender: " + gender);
+        Console.WriteLine("ID Number: " + idNum);
+        Console.WriteLine("Unique Employee Number: " + empNum);
+    }
+}
