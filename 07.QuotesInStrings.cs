8cdf70a4050625ea0aeb4ce85126c62b29f8a323
Nitroto <astodorov@outlook.com>
2015-03-10 15:37:04 +0200

    Create 07.QuotesInStrings.cs

---
+++ 07.QuotesInStrings.cs
@@ -0,0 +1,12 @@
+using System;
+
+class QuotesInStrings
+{
+    static void Main()
+    {
+        string firstString = "The \"use\" of quotations causes difficulties.";
+        string secondString = @"The ""use"" of quotations causes difficulties.";
+        Console.WriteLine(firstString);
+        Console.WriteLine(secondString);
+    }
+}
