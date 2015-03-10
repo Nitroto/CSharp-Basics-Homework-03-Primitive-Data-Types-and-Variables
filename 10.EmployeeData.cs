90abe100ef45a1106c6c48315eeb013251bb67e3
Nitroto <astodorov@outlook.com>
2015-03-10 16:11:51 +0200

    Update 10.EmployeeData.cs

--- 10.EmployeeData.cs
+++ 10.EmployeeData.cs
@@ -5,13 +5,13 @@ class EmployeeData
         string firstName = Console.ReadLine();
         string lastName = Console.ReadLine();
         ushort age = Convert.ToUInt16(Console.ReadLine());
-        string gender = Console.ReadLine();
+        char gender = Convert.ToChar(Console.ReadLine());
         uint idNum = Convert.ToUInt32(Console.ReadLine());
         ulong empNum = Convert.ToUInt64(Console.ReadLine());
         Console.WriteLine("Name: " + firstName);
         Console.WriteLine("Last Name: " + lastName);
         Console.WriteLine("Age: " + age);
-        Console.WriteLine("Gender: " + gender);
+        Console.WriteLine("Gender: {0}", (gender=='m'?"male" :"female"));
         Console.WriteLine("ID Number: " + idNum);
         Console.WriteLine("Unique Employee Number: " + empNum);
     }
