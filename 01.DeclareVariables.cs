3aa91a27d068d35e3e3e938e0f1377b8cf961dab
Nitroto <astodorov@outlook.com>
2015-03-10 15:32:22 +0200

    Create 01.DeclareVariables.cs

---
+++ 01.DeclareVariables.cs
@@ -0,0 +1,18 @@
+using System;
+
+  class DeclareVariables
+  {
+      static void Main()
+      {
+          ushort ush = 52130;
+          sbyte sb = -115;
+          uint ui = 4825932u;
+          byte b = 97;
+          short sh = -10000;
+          Console.WriteLine(ush);
+          Console.WriteLine(sb);
+          Console.WriteLine(ui);
+          Console.WriteLine(b);
+          Console.WriteLine(sh);
+      }
+  }
